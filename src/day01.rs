b53d589856b8f49da732ed8c3107f1e320d8eda1
teddylear <20077627+teddylear@users.noreply.github.com>
2023-12-20 21:08:18 -0500

    gottem

--- src/day01.rs
+++ src/day01.rs
@@ -5,8 +5,6 @@ pub fn day01(input_lines: &str) -> (String, String) {
     }
     let mut answer2 = 0;
 
-    // let first_line = input_lines.split("\n").collect::<Vec<&str>>()[0];
-    // println!("{}", first_line);
     for line in input_lines.split("\n") {
         answer2 = answer2 + calc_line_amount_with_num(line)
     }
@@ -27,28 +25,65 @@ fn calc_line_amount_with_num(line: &str) -> u32 {
                first_char = temp_char;
            }
            last_char = temp_char;
-        }
-
-        if c == 'e' && is_match_num_word(&index, line, "eight") {
+        } else if c == 't' && is_match_num_word(&index, line, "two") {
+           temp_char = get_number_from_word("two");
+           if first_char == 'a' {
+               first_char = temp_char;
+           }
+           last_char = temp_char;
+        } else if c == 't' && is_match_num_word(&index, line, "three") {
+           temp_char = get_number_from_word("three");
+           if first_char == 'a' {
+               first_char = temp_char;
+           }
+           last_char = temp_char;
+        } else if c == 'f' && is_match_num_word(&index, line, "four") {
+           temp_char = get_number_from_word("four");
+           if first_char == 'a' {
+               first_char = temp_char;
+           }
+           last_char = temp_char;
+        } else if c == 'f' && is_match_num_word(&index, line, "five") {
+           temp_char = get_number_from_word("five");
+           if first_char == 'a' {
+               first_char = temp_char;
+           }
+           last_char = temp_char;
+        } else if c == 's' && is_match_num_word(&index, line, "six") {
+           temp_char = get_number_from_word("six");
+           if first_char == 'a' {
+               first_char = temp_char;
+           }
+           last_char = temp_char;
+        } else if c == 's' && is_match_num_word(&index, line, "seven") {
+           temp_char = get_number_from_word("seven");
+           if first_char == 'a' {
+               first_char = temp_char;
+           }
+           last_char = temp_char;
+        } else if c == 'e' && is_match_num_word(&index, line, "eight") {
            temp_char = get_number_from_word("eight");
            if first_char == 'a' {
                first_char = temp_char;
            }
            last_char = temp_char;
-        }
-
-        // TODO: other first letters
-        if c.is_digit(10) {
+        } else if c == 'n' && is_match_num_word(&index, line, "nine") {
+           temp_char = get_number_from_word("nine");
+           if first_char == 'a' {
+               first_char = temp_char;
+           }
+           last_char = temp_char;
+        } else if c.is_digit(10) {
            if first_char == 'a' {
                first_char = c;
            }
            last_char = c;
         }
+
         index = index + 1;
     }
 
     let result_string = format!("{first_char}{last_char}");
-    // println!("result_string {}", result_string);
     if result_string == "aa" {
         return 0;
     }
@@ -61,8 +96,12 @@ fn is_match_num_word(index: &usize, line: &str, word: &str) -> bool {
         return false;
     }
 
+    let first_option = temp_line.find(word).unwrap();
+    if first_option == *index {
+        return true;
+    }
+
     let last_option = temp_line.rfind(word).unwrap();
-    println!("{}", last_option);
     if last_option == *index {
         return true;
     }
@@ -111,20 +150,21 @@ mod tests {
 
     #[test]
     fn check_day01_part2_case1() {
-        assert_eq!(calc_line_amount_with_num("9eightone"), 91) }
+        assert_eq!(calc_line_amount_with_num("9eightone"), 91)
+    }
 
-    // #[test]
-    // fn check_day01_part1_case1() {
-        // assert_eq!(day01("").0, "0".to_string())
-    // }
+    #[test]
+    fn check_day01_part2_case2() {
+        assert_eq!(calc_line_amount_with_num("9twopjqkghmbone"), 91)
+    }
 
-    // #[test]
-    // fn check_day01_part2_case1() {
-        // assert_eq!(day01("").1, "0".to_string())
-    // }
+    #[test]
+    fn check_day01_part2_case3() {
+        assert_eq!(calc_line_amount_with_num("hczsqfour3nxm5seven4"), 44)
+    }
 
-    // #[test]
-    // fn check_day01_both_case1() {
-        // assert_eq!(day01(""), ("0".to_string(), "0".to_string()))
-    // }
+    #[test]
+    fn check_day01_part2_case4() {
+        assert_eq!(calc_line_amount_with_num("hczsqfour3fourseven4"), 44)
+    }
 }
