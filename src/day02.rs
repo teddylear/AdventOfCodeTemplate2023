365c19a67622a3a2ad19c9bb32a3f4380f67bd9f
teddylear <20077627+teddylear@users.noreply.github.com>
2023-12-21 23:22:13 -0500

    more work day1

--- src/day02.rs
+++ src/day02.rs
@@ -2,27 +2,51 @@ use std::collections::HashMap;
 
 pub fn day02(input_lines: &str) -> (String, String) {
     let _ = input_lines;
-    let answer1 = 0;
-    // let answer1 = day02_part1(input_lines);
+    let answer1 = day02_part1(input_lines, HashMap::from([
+        ("red".to_string(), 12),
+        ("blue".to_string(), 14),
+        ("green".to_string(), 13),
+    ]));
     let answer2 = 0;
     (format!("{}", answer1), format!("{}", answer2))
 }
 
-// fn day02_part1(input_lines: &str) -> (usize) {
-    // let mut result = 0;
-    // for line in input_lines.split("\n") {
-        // result = result + value_per_game(line)
-    // }
-    // return result;
-// }
+fn day02_part1(input_lines: &str, cubes: HashMap<String, usize>) -> usize {
+    let mut result = 0;
+    for line in input_lines.split("\n") {
+        result = result + value_per_game(line, cubes.clone())
+    }
+    return result;
+}
 
-// fn value_per_game(input_lines: &str) -> (usize) {
+fn value_per_game(line: &str, cubes: HashMap<String, usize>) -> usize {
+    let mut game_num = 0;
+    let mut index = 0;
+    for piece in line.split(":") {
+        if index == 0 {
+            game_num = game_value_in_line(piece);
+        } else {
+            if !is_valid_game(piece, cubes.clone()) {
+                return 0;
+            }
+        }
+        index = index + 1;
+    }
+    return game_num;
+}
 
-// }
-
-fn game_value_in_line(game_text: &str) -> usize {
-    let game_num_str = game_text.strip_prefix("Game ").unwrap();
+fn game_value_in_line(game_num_text: &str) -> usize {
+    let game_num_str = game_num_text.strip_prefix("Game ").unwrap();
     return game_num_str.parse::<usize>().unwrap();
+}
+
+fn is_valid_game(game_text: &str, cubes: HashMap<String, usize>) -> bool {
+    for game_set in game_text.split(";") {
+        if !is_valid_set(set_str_to_map(game_set), cubes.clone()) {
+            return false;
+        }
+    }
+    return true;
 }
 
 fn is_valid_set(set: HashMap<String, usize>, cubes: HashMap<String, usize>) -> bool {
@@ -45,10 +69,12 @@ fn set_str_to_map(set_str: &str) -> HashMap<String, usize> {
     let mut color: String;
 
     for num_color_combo in set_str.split(", ") {
+        println!("{}", num_color_combo);
         index = 0;
         num = 0;
         color = "".to_string();
         for piece in num_color_combo.split(" ") {
+            println!("{}", piece);
             if index == 0 {
                 num = piece.parse::<usize>().unwrap();
             } else {
